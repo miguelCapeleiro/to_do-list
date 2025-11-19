1e60bf0024b69f4f0b92b15d9051e08bc466f44c
Miguel <miguelcapeleiro2025@gmail.com>
2025-11-19 11:27:23 -0300

    tabela do github
    upar na tabela

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from main.views import TaskList
 urlpatterns = [
-    
+    path('main', TaskList.as_view(), name='task_list')                                                                                
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Task
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
     
+class TaskList(ListView):
+    model = Task 
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tarefas'
