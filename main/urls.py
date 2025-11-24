bc0737cf64031070762bd28b2b3d59b34f1b8319
skaoz <mateus.araujo.banana@gmail.com>
2025-11-24 11:41:20 -0300

    novas url
    coloquei novas urls

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from main.views import TaskList
+from main.views import TaskList 
+from . import views
 urlpatterns = [
-    path('main', TaskList.as_view(), name='task_list')                                                                                
+    path('main', TaskList.as_view(), name='task_list'),            
+    path('funcao/', views.task_list, name='task_list_funcao'),
+    path('concluido', views.task_concluido, name='task_list_concluido'),
+    path('teste', views.teste, name='teste')
+                                                            
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,36 @@ class TaskList(ListView):
     model = Task 
     template_name = 'tasks/task_list.html'
     context_object_name = 'tarefas'
+    
+
+def task_list(request):
+    tarefas = Task.objects.all() 
+    context = {
+        'tarefas':tarefas,
+        'titulo_pagina': 'Minhas Tarefas'
+    }
+    return render(request, 'tasks/task_list.html', context)
+
+def task_concluido(request):
+    a = 4
+    b = 5
+    c = a + b
+
+    tarefas = Task.objects.filter(concluida=1   ) 
+    context = {
+        'tarefas':tarefas,
+        'titulo_pagina':'Minhas Tarefas Concluidas'
+
+    }
+    return render(request, 'tasks/concluido.html', context)
+
+
+def teste(request):
+
+    tarefas = Task.objects.filter(concluida=1) 
+    context = {
+        'tarefas':tarefas,
+        'titulo_pagina': 'Teste'
+
+    }
+    return render(request, 'tasks/aaaa.html', context)
